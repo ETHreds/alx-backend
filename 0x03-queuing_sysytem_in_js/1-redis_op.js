075cdefee2c3f3a1be3f982c1913b16c2af8baae
ETHreds <jcmaglaw@gmail.com>
2024-02-07 15:01:36 +0300

    Async business

---
+++ 0x03-queuing_sysytem_in_js/1-redis_op.js
@@ -0,0 +1,41 @@
+// Import necessary modules
+import redis from 'redis';
+
+// Create a Redis client
+const client = redis.createClient();
+
+// Event handler for successful connection
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+// Event handler for connection error
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err.message}`);
+});
+
+// Close the Redis connection on script termination
+process.on('SIGINT', () => {
+  client.quit();
+});
+
+// Function to set a new school value in Redis
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, redis.print);
+}
+
+// Function to display the value for a given school key
+function displaySchoolValue(schoolName) {
+  client.get(schoolName, (err, value) => {
+    if (err) {
+      console.log(`Error getting value for ${schoolName}: ${err.message}`);
+    } else {
+      console.log(`Value for ${schoolName}: ${value}`);
+    }
+  });
+}
+
+// Call the functions
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
---
+++ 0x03-queuing_sysytem_in_js/2-redis_op_async.js
@@ -0,0 +1,46 @@
+// Import the necessary modules
+import redis from 'redis';
+import { promisify } from 'util';
+
+/**
+ * Creates a new Redis client to interact with a Redis server.
+ * @type {RedisClient}
+ */
+const client = redis.createClient();
+
+/**
+ * Promisified version of the `client.get` function with Redis client binding.
+ * @type {function}
+ */
+const getAsync = promisify(client.get).bind(client);
+
+/**
+ * Sets a new value for a school in Redis.
+ * @param {string} schoolName - The name of the school.
+ * @param {string} value - The value to set for the school.
+ * @returns {Promise<void>} A promise that resolves after the operation is complete.
+ */
+async function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, redis.print);
+}
+
+/**
+ * Retrieves and displays the value for a school using async/await.
+ * @param {string} schoolName - The name of the school to retrieve the value for.
+ * @returns {Promise<void>} A promise that resolves after the operation is complete.
+ */
+async function displaySchoolValue(schoolName) {
+  try {
+    const value = await getAsync(schoolName);
+    console.log(`Value for ${schoolName}: ${value}`);
+  } catch (err) {
+    console.error(`Error getting value for ${schoolName}: ${err.message}`);
+  }
+}
+
+
+(async () => {
+    await displaySchoolValue('Holberton');
+    setNewSchool('HolbertonSanFrancisco', '100');
+    await displaySchoolValue('HolbertonSanFrancisco');
+  })();
